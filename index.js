b3b498a3c9631787e3a80d3b41ed0691743e7bec
Indu Ramesh <induramesh@gmail.com>
2018-09-06 20:48:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,7 +43,8 @@ function createRock(x) {
   rock.className = 'rock'
   rock.style.left = `${x}px`
 
-  var top = rock.style.top = 0
+  var top = 0
+  rock.style.top = top
 
   GAME.appendChild(rock)
 
